1bc46de0739bbf9d5cde620dc2bb443209560501
JBerov <jballer878@gmail.com>
2020-06-30 18:31:20 +0300

    Parsing config

--- init.js
+++ init.js
@@ -1,5 +1,27 @@
 var params = process.argv;
-var config = require('config.json');
+var config = require('./config.json');
+
+
+
+if(params.length != 3)
+  return console.log("Improper parameters specified. Please start the server like this: 'node init.js serverKey' ");
+ 
+
+var serverData = config["servers"][params[2]];
+
+if(serverData)
+{
+  global.serverId = params[2];
+  global.serverSpecs = serverData;
+}
+else
+{
+  var message =  "Invalid server. Available servers are: ";
+  for(var i in config["servers"])
+    message += i + " ";
+  return console.log(message);
+}
+  
 /*var io = require('socket.io')();
 
 io.origins(["*:*"]);
@@ -8,4 +30,4 @@ io.on('connection', client => {
   client.on('disconnect', () => { console.log("user disconnected"); });
 });
 io.listen(9903);*/
-console.log("ShootThis-Backend running " + params[2] + " on port 9903");
+console.log("ShootThis-Backend running " + params[2] + " on port " + serverData["port"]);
